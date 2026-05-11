681b8c0d3aa3b64c24b17ec157d6df2375bf9673
Sunita9840 <sunitapal9840@gmail.com>
2026-05-11 11:46:58 +0530

    app_web.py

--- app_web.py
+++ app_web.py
@@ -26,25 +26,30 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-BaseOptions = mp.tasks.BaseOptions
-FaceMesh = mp.tasks.vision.FaceLandmarker
-FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
+# BaseOptions = mp.tasks.BaseOptions
+# FaceMesh = mp.tasks.vision.FaceLandmarker
+# FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+# VisionRunningMode = mp.tasks.vision.RunningMode
 
-options = FaceLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path="face_landmarker.task"),
-    running_mode=VisionRunningMode.IMAGE
-)
+# options = FaceLandmarkerOptions(
+#     base_options=BaseOptions(model_asset_path="face_landmarker.task"),
+#     running_mode=VisionRunningMode.IMAGE
+# )
 
-face_mesh = FaceLandmarker.create_from_options(options)
+# face_mesh = FaceLandmarker.create_from_options(options)
 # ---------------- UI ----------------
 st.title("AI Virtual Try-On (Web Version)")
 
 run = st.checkbox("Start Camera")
-cap = cv2.VideoCapture(0)
+
 
 # ---------------- MEDIAPIPE ----------------
-
+mp_face_mesh = mp.solutions.face_mesh.FaceMesh(
+    max_num_faces=1,
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
 # ---------------- LOAD IMAGES ----------------
 glasses = cv2.imread("assets/glasses/g1.png", cv2.IMREAD_UNCHANGED)
 hat = cv2.imread("assets/hats/h1.png", cv2.IMREAD_UNCHANGED)
@@ -52,6 +57,8 @@ necklace = cv2.imread("assets/necklaces/n1.png", cv2.IMREAD_UNCHANGED)
 
 # ---------------- OVERLAY FUNCTION ----------------
 def overlay(frame, overlay_img, x, y, w, h):
+    if overlay_img is None:
+        return frame
 
     overlay_img = cv2.resize(overlay_img, (w, h))
 
@@ -74,65 +81,123 @@ def overlay(frame, overlay_img, x, y, w, h):
             )
 
     return frame
+# ---------------- PLACEHOLDER ----------------
+frame_placeholder = st.image([])
 
 # ---------------- CAMERA ----------------
 cap = cv2.VideoCapture(0)
 
-frame_placeholder = st.image([])
+# frame_placeholder = st.image([])
+if run:
+     while cap.isOpened():
 
-# ---------------- MAIN LOOP ----------------
-while run:
+        ret, frame = cap.read()
+        if not ret:
+            st.warning("Camera not working")
+            break
 
-    ret, frame = cap.read()
-    if not success:
-        st.warning("Camera not working")
-        break
+        frame = cv2.flip(frame, 1)
+        h, w, _ = frame.shape
 
-    frame = cv2.flip(frame, 1)
-    h, w, _ = frame.shape
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = face_mesh.process(frame)
+        results = mp_face_mesh.process(rgb)
 
-    if results.multi_face_landmarks:
+        if results.multi_face_landmarks:
 
-        for face in results.multi_face_landmarks:
+            for face in results.multi_face_landmarks:
 
-            # -------- EYES --------
-            left = face.landmark[33]
-            right = face.landmark[263]
+                # -------- EYES --------
+                left = face.landmark[33]
+                right = face.landmark[263]
 
-            x1, y1 = int(left.x * w), int(left.y * h)
-            x2, y2 = int(right.x * w), int(right.y * h)
+                x1, y1 = int(left.x * w), int(left.y * h)
+                x2, y2 = int(right.x * w), int(right.y * h)
 
-            gw = int(abs(x2 - x1) * 2.2)
-            gh = int(gw * 0.5)
+                gw = int(abs(x2 - x1) * 2.2)
+                gh = int(gw * 0.5)
 
-            gx = int(x1 - gw * 0.25)
-            gy = int(y1 - gh * 0.5)
+                gx = int(x1 - gw * 0.25)
+                gy = int(y1 - gh * 0.5)
 
-            frame = overlay(frame, glasses, gx, gy, gw, gh)
+                frame = overlay(frame, glasses, gx, gy, gw, gh)
 
-            # -------- HAT --------
-            forehead = face.landmark[10]
+                # -------- HAT --------
+                forehead = face.landmark[10]
 
-            fx, fy = int(forehead.x * w), int(forehead.y * h)
+                fx, fy = int(forehead.x * w), int(forehead.y * h)
 
-            hw = int(gw * 1.5)
-            hh = int(hw * 0.8)
+                hw = int(gw * 1.5)
+                hh = int(hw * 0.8)
 
-            frame = overlay(frame, hat, fx - hw//2, fy - hh - 30, hw, hh)
+                frame = overlay(frame, hat, fx - hw//2, fy - hh - 30, hw, hh)
 
-            # -------- NECKLACE --------
-            shoulder = face.landmark[152]
+                # -------- NECKLACE --------
+                shoulder = face.landmark[152]
 
-            nx, ny = int(shoulder.x * w), int(shoulder.y * h)
+                nx, ny = int(shoulder.x * w), int(shoulder.y * h)
 
-            nw = int(gw * 1.2)
-            nh = int(nw * 0.6)
+                nw = int(gw * 1.2)
+                nh = int(nw * 0.6)
 
-            frame = overlay(frame, necklace, nx - nw//2, ny + 20, nw, nh)
+                frame = overlay(frame, necklace, nx - nw//2, ny + 20, nw, nh)
 
-    frame_placeholder.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        frame_placeholder.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
 cap.release()
+# # ---------------- MAIN LOOP ----------------
+# while run:
+
+#     ret, frame = cap.read()
+#     if not success:
+#         st.warning("Camera not working")
+#         break
+
+#     frame = cv2.flip(frame, 1)
+#     h, w, _ = frame.shape
+
+#     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     results =mp_face_mesh.process(rgb_frame)
+
+#     if results.multi_face_landmarks:
+
+#         for face in results.multi_face_landmarks:
+
+#             # -------- EYES --------
+#             left = face.landmark[33]
+#             right = face.landmark[263]
+
+#             x1, y1 = int(left.x * w), int(left.y * h)
+#             x2, y2 = int(right.x * w), int(right.y * h)
+
+#             gw = int(abs(x2 - x1) * 2.2)
+#             gh = int(gw * 0.5)
+
+#             gx = int(x1 - gw * 0.25)
+#             gy = int(y1 - gh * 0.5)
+
+#             frame = overlay(frame, glasses, gx, gy, gw, gh)
+
+#             # -------- HAT --------
+#             forehead = face.landmark[10]
+
+#             fx, fy = int(forehead.x * w), int(forehead.y * h)
+
+#             hw = int(gw * 1.5)
+#             hh = int(hw * 0.8)
+
+#             frame = overlay(frame, hat, fx - hw//2, fy - hh - 30, hw, hh)
+
+#             # -------- NECKLACE --------
+#             shoulder = face.landmark[152]
+
+#             nx, ny = int(shoulder.x * w), int(shoulder.y * h)
+
+#             nw = int(gw * 1.2)
+#             nh = int(nw * 0.6)
+
+#             frame = overlay(frame, necklace, nx - nw//2, ny + 20, nw, nh)
+
+#     frame_placeholder.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+# cap.release()
